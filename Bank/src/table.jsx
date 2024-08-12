b982d95fe746af06826369134057a05a9b62167d
Tonny Bett <betttonny26@gmail.com>
2024-08-13 00:22:51 +0300

    create a transaction table for the transactions

---
+++ Bank/src/table.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const TransactionTable = ({ transactions }) => {
+  return (
+    <table>
+        <thead>
+            <tr>
+                <th>Description</th>
+                <th>Amount</th>
+            </tr>
+        </thead>
+    <tbody>
+        {transactions.map((transaction, index) => (
+          <tr key={index}>
+            <td>{transaction.description}</td>
+            <td>{transaction.amount}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionTable;
